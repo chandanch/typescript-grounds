f950df0756dd10022eb7c97cd9eb48f404ad2f38
Chandan CH <chandanch@users.noreply.github.com>
2017-10-06 19:51:25 +0530

    covers essential oop concepts

---
+++ playground1.ts
@@ -0,0 +1,52 @@
+/**
+ *@description This playground covers the concept of inheritance,
+ * Using Get and Set Method
+ * Creating constutors
+ * Role of super keyword
+ * Use of Static keyword
+ */
+class Car {
+    protected brakeType: string = "drums";
+    public static fuelType: string;
+    constructor(protected vehicleType: string) {
+        console.log(vehicleType)
+    }
+    startEngine() {
+        alert(`Started simple Car engine`);
+    }
+
+    showVehicleType() {
+        alert(`Vehicle type: ${this.vehicleType}`);
+    }
+    static showFuelType() {
+        alert(`Fuel Type ${this.fuelType}`);
+    }
+}
+
+class Lamborghini extends Car {
+
+    constructor(protected vehicleType: string, public model: string) {
+        super(vehicleType);
+        this.brakeType = "abs";
+    }
+    startEngine() {
+        alert(`Started V-12 340 NM engine`);
+        super.startEngine();
+    }
+
+    get brake() {
+        return this.brakeType;
+    }
+    set brake(brakeType: string) {
+        this.brakeType = brakeType;
+    }
+}
+
+var lamborghini = new Lamborghini("Sports", "aventador");
+lamborghini.startEngine();
+lamborghini.showVehicleType();
+lamborghini.brake = "Antilock brakes";
+alert(`Brakes Type: ${lamborghini.brake}`);
+Car.fuelType = "disel";
+Car.showFuelType();
+
