f5a908d02824b0f7ab7ace877080ef5e5aef8458
Chandan CH <chandanch@users.noreply.github.com>
2017-10-09 13:24:25 +0530

    methods definition and implementation

--- playground6.ts
+++ playground6.ts
@@ -13,6 +13,7 @@ interface ISimpleInterest {
     principal: number;
     rate: number;
     time: number;
+    showCurrentDate(): string;
 }
 
 class BankAccount {
@@ -37,15 +38,20 @@ sueAccount.getCustomerDetails(sueDetails);
 
 class LoanAccount {
     public simpleInterest: ISimpleInterest;
+    private date = new Date();
 
     calculateSimpleInterest() {
         this.simpleInterest = {
             principal: 34000,
             rate: 12.3,
-            time: 40
+            time: 40,
+            showCurrentDate: () => {
+                return this.date.toString();
+            }
         }    
         var si = (this.simpleInterest.principal * this.simpleInterest.rate * this.simpleInterest.time) / 100;
         alert(`Simple interest is ${si}`);
+        alert(`${this.simpleInterest.showCurrentDate()}`);
     }
 }
 
