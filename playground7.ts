687228b3d47b821ca9c5a3f8621d66efe19edbb6
Chandan CH <chandanch@users.noreply.github.com>
2017-10-09 13:31:57 +0530

    usage of union types in interface

---
+++ playground7.ts
@@ -0,0 +1,27 @@
+/**
+ *@description usage of Union Types in interface
+ * interface
+ *@author chandanch
+ */
+
+interface IPerson {
+    firstName: string;
+    lastName: string;
+    age: number | number[] | string;
+}
+
+var customer: IPerson;
+customer = {
+    firstName: "Fathima",
+    lastName: "Raiz",
+    age: 23
+}
+//alert(JSON.stringify(customer));
+
+var twins: IPerson;
+twins = {
+    firstName: "David",
+    lastName: "lodman",
+    age: [23,44]
+}
+alert(JSON.stringify(twins));
