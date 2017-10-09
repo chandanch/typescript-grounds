fcafe2ff2b464eeb411c8b4655a700d546d6c752
Chandan CH <chandanch@users.noreply.github.com>
2017-10-09 13:12:08 +0530

    declaring variables of custom type

---
+++ playground6.ts
@@ -0,0 +1,53 @@
+/**
+ *@description definining variables of custom types using
+ * interface
+ */
+
+interface IPerson {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+interface ISimpleInterest {
+    principal: number;
+    rate: number;
+    time: number;
+}
+
+class BankAccount {
+
+    getCustomerDetails(customer: IPerson) {
+        alert(
+            `
+                Customer details: \n
+                Customer Name: ${customer.firstName} ${customer.lastName}
+                Customer Age: ${customer.age}
+            `
+        )
+    }
+}
+var sueAccount = new BankAccount();
+var sueDetails = {
+    firstName: "Sue",
+    lastName: "Heck",
+    age: 23
+}
+sueAccount.getCustomerDetails(sueDetails);
+
+class LoanAccount {
+    public simpleInterest: ISimpleInterest;
+
+    calculateSimpleInterest() {
+        this.simpleInterest = {
+            principal: 34000,
+            rate: 12.3,
+            time: 40
+        }    
+        var si = (this.simpleInterest.principal * this.simpleInterest.rate * this.simpleInterest.time) / 100;
+        alert(`Simple interest is ${si}`);
+    }
+}
+
+var loanAccount = new LoanAccount();
+loanAccount.calculateSimpleInterest();
