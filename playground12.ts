e4ba2d5f926c7a1d70228d18bb908bda55d613e6
Chandan CH <chandanch@users.noreply.github.com>
2017-10-16 10:14:58 +0530

    use of constants

---
+++ playground12.ts
@@ -0,0 +1,9 @@
+/**
+ *@description use of constants
+ */
+const interestRate = 4.5;
+let balance = 4500;
+let newBalance = balance + interestRate;
+alert(`Your Balance: ${newBalance}`);
+// Cannot change interestRate since its constant
+//interestRate = 3.5
