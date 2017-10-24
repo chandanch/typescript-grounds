d3546491a82d42f24e69033244c443cb275165ab
Chandan CH <chandanch@users.noreply.github.com>
2017-10-24 21:21:59 +0530

    changed variable name

--- singleton-pattern.ts
+++ singleton-pattern.ts
@@ -1,20 +1,20 @@
 class Car {
     // A variable which stores the singleton object. Intially,
     // the variable acts like a placeholder
-    private static _car: Car = null;
+    private static __car: Car = null;
     // private constructor so that no instance is created
     private constructor() {
     }
     // This is how we create a singleton object
     public static getInstance(): Car {
         // check if an instance of the class is already created
-        if (this._car == null) {
+        if (this.__car == null) {
             // If not created create an instance of the class
             // store the instance in the variable
-            this._car = new Car();
+            this.__car = new Car();
         }
         // return the singleton object
-        return this._car
+        return this.__car
     }
     public getCarName(name: string) {
         alert(`Car is ${name}`)
