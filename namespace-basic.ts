3571f4c73db3b5c35e82d032d8e1caa919fe48bc
Chandan CH <chandanch@users.noreply.github.com>
2017-10-25 21:21:50 +0530

    using namespace

---
+++ namespace-basic.ts
@@ -0,0 +1,15 @@
+namespace CarNameSpace {
+    export class Car {
+        public carType: string;
+        public engine: string;
+
+        getCarInfo() {
+            alert(`Car type: ${this.carType} ${this.engine}`);
+        }
+    }
+}
+
+var honda = new CarNameSpace.Car();
+honda.carType = "Civic";
+honda.engine = "v8";
+honda.getCarInfo();
