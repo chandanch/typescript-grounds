a689a379d0749bead51e1809a6b8e45a898cd7c7
Chandan CH <chandanch@users.noreply.github.com>
2017-10-29 23:24:07 +0530

    creating arrays to store integer value

---
+++ arrays-playground2.ts
@@ -0,0 +1,27 @@
+/**
+ *@description creating integer arrays and displaying the items of array
+ */
+class Players {
+    // Defining an array of type string
+    private playerNames: string[] = [];
+    //Defining an array of type string
+    private playerIds: number[] = [];
+
+    constructor(playerName: string, playerId: number) {
+        this.playerIds.push(playerId);
+        this.playerNames.push(playerName)
+    }
+
+    showPlayers() {
+        let playerNames: string
+        for (let i = 0; i < this.playerNames.length; i++) {
+            playerNames = `${i}: ${this.playerNames[i]}`
+        } 
+        alert(`Player Names:\n ${this.playerNames}`);
+        alert(`Player IDs: ${this.playerIds}`)
+    }
+}
+var john = new Players('John', 12);
+var peter = new Players('Peter', 23);
+peter.showPlayers();
+
