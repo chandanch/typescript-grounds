af56639358c407d263badd3bae1004e4dcbe7913
Chandan CH <chandanch@users.noreply.github.com>
2017-11-28 19:17:59 +0530

    use of tuples

---
+++ tuples.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Tuples are similar to array
+ * we specify the types for the first two items of the array
+ */
+
+// creating tuples
+let randomValues: [string, number] = ['Stack', 1000];
+
+// adding more items to tuples it should have the same types as specified
+randomValues[2] = 'Samules';
+randomValues[3] = 34;
