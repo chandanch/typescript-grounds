4f0f8a7532de50838c74f970aca2f172879b2214
Chandan CH <chandanch@users.noreply.github.com>
2017-12-13 20:24:21 +0530

    using type parameters for array

---
+++ type-parameters-array.ts
@@ -0,0 +1,13 @@
+/**
+ * Using type parameters for array
+ * Format: Array<T>: `T` is a placeholder for type
+ * Type parameters specify the type that the array can use
+ */
+
+let popularCars: Array<string>;
+popularCars = ['Mazda R2', 'StonisGT', 'Camera'];
+alert(popularCars);
+
+let popularCarColors = new Array<string>(4);
+popularCarColors = ['red', 'stealth black', 'tourquise', ''];
+alert(popularCarColors);
