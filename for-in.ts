e95277dcd2fc86056a320475f1dc7a00363f93a8
Chandan CH <chandanch@users.noreply.github.com>
2017-12-20 18:02:12 +0530

    use of for-in loop

---
+++ for-in.ts
@@ -0,0 +1,21 @@
+/**
+ * @desc for-in loop
+ * Used for iterating over the object properties
+ */
+let scores = [12, 34, 45, 56, 44, 78, 88, 99];
+
+let car = {
+    model: 'chevy55',
+    price: 45,
+    engineType: 'V8',
+    compression: 'DRF'
+}
+
+// interating over object properties
+for (let key in car) {
+    console.log(`Keys: ${key}`);
+}
+
+for (let index in scores) {
+    console.log(`Index: ${index}`);
+}
