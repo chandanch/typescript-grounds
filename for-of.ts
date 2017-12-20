c2c725d40ad52850a106a81fce0e590347bd2acf
Chandan CH <chandanch@users.noreply.github.com>
2017-12-20 18:08:41 +0530

    use of for-of loop

---
+++ for-of.ts
@@ -0,0 +1,10 @@
+/**
+ * @desc for-of loop
+ * Used for looping through array elements
+ * return, break & continue statements 
+ */
+let scores = [12, 34, 45, 56, 44, 78, 88, 99];
+
+for (let score of scores) {
+    console.log(`Score: ${score}`);
+}
