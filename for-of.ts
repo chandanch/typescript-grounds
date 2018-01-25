cdad8bf3c4d8de5205d00afcbf496cf23fe798dd
Chandan CH <chandanch@users.noreply.github.com>
2018-01-25 12:19:59 +0530

    update desc for for-of

--- for-of.ts
+++ for-of.ts
@@ -1,7 +1,6 @@
 /**
  * @desc for-of loop
- * Used for looping through array elements
- * return, break & continue statements 
+ * Used for looping through Iterable elements
  */
 let scores = [12, 34, 45, 56, 44, 78, 88, 99];
 
