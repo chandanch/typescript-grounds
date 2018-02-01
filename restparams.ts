b858b2db35192ba25224970a88a9731efbd0b3de
Chandan CH <chandanch@users.noreply.github.com>
2018-02-01 17:02:05 +0530

    update desc restparams

--- restparams.ts
+++ restparams.ts
@@ -1,6 +1,7 @@
 /**
  * @desc Rest parameters allows to specify a variable number of parameters
  * to a function
+ * Rest parameters specify the rest of parameters to a function
  * @author chandanch
  */
 function showCarsOwned(customerName: string, ...cars: string[]) {
